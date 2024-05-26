07f714fbe2a32be57e5e5f33165f6210344af270
Sara Verdigi <saraverdigi3@gmail.com>
2024-05-26 23:39:08 +0200

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/constants/colors.dart';
+import 'package:to_do_app/widgets/todo_item.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 50, bottom: 20),
+                    child: Text(
+                      'All ToDo',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                  ),
+                  TodoItem()
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+AppBar _buildAppBar() {
+  return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 35,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('images/FotoMe.jpg'),
+            ),
+          ),
+        ],
+      ));
+}
+
+Widget searchBox() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey)),
+    ),
+  );
+}
---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,49 @@
+import '../constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/constants/colors.dart';
+
+class TodoItem extends StatelessWidget {
+  const TodoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+          Icons.check_box,
+          color: tdBlue,
+        ),
+        title: Text(
+          'Check Mail',
+          style: TextStyle(
+              fontSize: 16,
+              color: tdBlack,
+              decoration: TextDecoration.lineThrough),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5)
+          ),
+          child: IconButton(
+            onPressed: (){},
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+
+          ),
+        ),
+      ),
+    );
+  }
+}
